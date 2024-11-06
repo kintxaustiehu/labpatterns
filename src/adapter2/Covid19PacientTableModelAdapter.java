35d1efd63bbd12652dd34d18d119365e59b8d1df
kintxaustiehu <kintxausti001@ikasle.ehu.eus>
2024-11-06 16:11:03 +0100

    Format Covid19PacientTableModelAdapter

--- src/adapter2/Covid19PacientTableModelAdapter.java
+++ src/adapter2/Covid19PacientTableModelAdapter.java
@@ -5,31 +5,30 @@ import javax.swing.table.AbstractTableModel;
 import domain.Covid19Pacient;
 
 public class Covid19PacientTableModelAdapter extends AbstractTableModel {
-	  protected Covid19Pacient pacient;
-	  protected String[] columnNames =
-	    new String[] {"Symptom", "Weight" };
+	protected Covid19Pacient pacient;
+	protected String[] columnNames = new String[] { "Symptom", "Weight" };
 
-	  public Covid19PacientTableModelAdapter(Covid19Pacient p) {
-	    this.pacient=p;
-	  }
-
-	  public int getColumnCount() {
-	    // Challenge!
-		 return 1;
-	  }
-
-	  public String getColumnName(int i) {
-	    // Challenge!
-		  return "Column name 1";
-	  }
-
-	  public int getRowCount() {
-	    // Challenge!
-		  return 1;
-	  }
-
-	  public Object getValueAt(int row, int col) {
-	    // Challenge!
-		  return "value";
-	  }
+	public Covid19PacientTableModelAdapter(Covid19Pacient p) {
+		this.pacient = p;
 	}
+
+	public int getColumnCount() {
+		// Challenge!
+		return 1;
+	}
+
+	public String getColumnName(int i) {
+		// Challenge!
+		return "Column name 1";
+	}
+
+	public int getRowCount() {
+		// Challenge!
+		return 1;
+	}
+
+	public Object getValueAt(int row, int col) {
+		// Challenge!
+		return "value";
+	}
+}
